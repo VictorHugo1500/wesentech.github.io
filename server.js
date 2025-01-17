120d9a02a2a79bc91f2569492a28ef2ca547985e
VictorHugo1500 <doutorschwarz1500@gmail.com>
2025-01-17 15:05:53 -0300

    Upload the Website Server

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
+const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.json());
